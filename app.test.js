7877ff8be34852e2ef11c2610dd7834183ef0b68
Dilshoda Abdualimova <dilshoda@Dilshodas-MacBook-Pro.local>
2023-04-12 19:48:48 -0700

    test: add sample tests

--- app.test.js
+++ app.test.js
@@ -1,3 +1,5 @@
+//  test for circleCollidesWithRectangle function
+
 describe("circleCollidesWithRectangle", () => {
     const circle = {
         position: { x: 5, y: 5 },
@@ -49,5 +51,55 @@ describe("circleCollidesWithRectangle", () => {
             velocity: { x: -1, y: -1 }
         };
         expect(circleCollidesWithRectangle({ circle: newCircle, rectangle })).toBe(true);
+    });
+});
+
+// test for Player 
+describe("Player", () => {
+    let player;
+
+    beforeEach(() => {
+        player = new Player({
+            position: { x: 10, y: 10 },
+            velocity: { x: 2, y: 2 }
+        });
+    });
+
+    it("should have the correct initial position, velocity, and radius", () => {
+        expect(player.position).toEqual({ x: 10, y: 10 });
+        expect(player.velocity).toEqual({ x: 2, y: 2 });
+        expect(player.radius).toBe(15);
+    });
+
+    it("should draw a yellow circle on the canvas", () => {
+        const mockBeginPath = jest.fn();
+        const mockArc = jest.fn();
+        const mockFillStyle = jest.fn();
+        const mockFill = jest.fn();
+        const mockClosePath = jest.fn();
+
+        const mockContext = {
+            beginPath: mockBeginPath,
+            arc: mockArc,
+            fillStyle: mockFillStyle,
+            fill: mockFill,
+            closePath: mockClosePath
+        };
+
+        player.draw(mockContext);
+
+        expect(mockBeginPath).toHaveBeenCalledTimes(1);
+        expect(mockBeginPath).toHaveBeenCalledWith();
+        expect(mockArc).toHaveBeenCalledTimes(1);
+        expect(mockArc).toHaveBeenCalledWith(10, 10, 15, 0, Math.PI * 2);
+        expect(mockFillStyle).toHaveBeenCalledTimes(1);
+        expect(mockFillStyle).toHaveBeenCalledWith("yellow");
+        expect(mockFill).toHaveBeenCalledTimes(1);
+        expect(mockClosePath).toHaveBeenCalledTimes(1);
+    });
+
+    it("should update the player's position", () => {
+        player.update();
+        expect(player.position).toEqual({ x: 12, y: 12 });
     });
 });
